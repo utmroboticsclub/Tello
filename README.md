6c703bfda158895821a868341ca2049023ccb6d1
Kyrel Jerome <kyrel2007@gmail.com>
2020-02-07 17:40:03 -0500

    controller

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Controls and performs various tasks using a Tello drone.
 
 ## Objectives
-
+The following are projects to be completed.
 ### Tello_UTM_Framework
 A framework for interfacing with the DJI Tello using Command based
 scheduling and calling.
---
+++ face_follow/controllers.py
@@ -0,0 +1,42 @@
+import Tello
+import time
+
+class TelloController:
+    def __init__(self):
+        self.tello = Tello()
+    def run(self):
+        # Tello program logic here
+
+
+class TelloPID:
+    # A PID object for use with
+    def __init__(kP=0, kI=0, kD=0):
+        self.kP = kP
+        self.kI = kI 
+        self.kD = kD
+        self.p = 0
+        self.i = 0
+        self.d = 0
+        self.current_value = 0
+        self.last_value = 0
+        self.last_time = time.time_ns
+
+    def start(targetVal):
+        self.targetVal = targetVal
+
+    def update(value):
+        self.last_value = self.current_value
+        self.current_value = value
+        self.last_time = self.current_time
+        self.current_time = time.time_ns
+        self.p = self.current_value - self.last_value
+        self.update_time_delta = self.current_time - self.last_time
+        if kI != 0:
+            self.i += p
+        if self.update_time_delta > 0:
+            self.d = self.kD*(self.current_value - self.last_value)/(self.update_time_delta)
+        self.output = self.kP*self.p + self.kI*self.i - self.kD*self.d
+        return self.output
+
+    def getOutput()
+        return self.output
